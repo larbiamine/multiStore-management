0fe77f9c0dfb044bd55d2f7365b337c683dd68a1
Amine Larbi <amine.larbi@icosnet.dz>
2024-05-10 23:34:34 +0100

    Add MyConfigModule to PrismaModule and update databaseUrl in PrismaClientManager and PrismaService

--- src/prisma/prisma.provider.ts
+++ src/prisma/prisma.provider.ts
@@ -1,11 +1,12 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { Request } from 'express';
+import { MyConfigService } from 'src/config/config.service';
 
 @Injectable()
 export class PrismaClientManager implements OnModuleDestroy {
   private clients: { [key: string]: PrismaClient } = {};
-
+  private configService: MyConfigService
   getStoreId(request: Request):string {
     return request["storeId"];
   }
@@ -16,7 +17,8 @@ export class PrismaClientManager implements OnModuleDestroy {
     let client = this.clients[storeId];
 
     if (!client) {
-      const databaseUrl = process.env.DATABASE_URL!.replace('public', storeId);
+      // const databaseUrl = process.env.DATABASE_URL!.replace('public', storeId);
+      const databaseUrl = this.configService.getDbUrl().replace('public', storeId);
 
       client = new PrismaClient({
         datasources: {
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,15 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
+import { MyConfigService } from 'src/config/config.service';
 const fs = require('fs');
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
+
   private prisma: PrismaClient;
-  constructor() {
+  constructor(private configService: MyConfigService) {
     super();
     this.prisma = new PrismaClient();
   }
@@ -31,21 +33,21 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
 
       return res;
     } catch (error) {
-      console.log('🆘 || error:', error);
+      console.log(`🆘 || error: createTable ${tableName} `, error);
       return false;
     }
   }
-  async runRelations(fileName: string, storeId:string): Promise<number | boolean> {
+  async runRelations(fileName: string, schemaName:string): Promise<number | boolean> {
+
     try {
       const filePath = `./src/database/tableCreations/${fileName}.sql`;
       let sqlScript = fs.readFileSync(filePath, 'utf8');
-      sqlScript = sqlScript.replace(`"Product"`, `${storeId}."Product"`);      // const res = await this.prisma.$executeRaw(sqlScript);
-      sqlScript = sqlScript.replace(`"Order"`, `${storeId}."Order"`);      // const res = await this.prisma.$executeRaw(sqlScript);
+      sqlScript = sqlScript.replace(`"Product"`, `${schemaName}."Product"`);      // const res = await this.prisma.$executeRaw(sqlScript);
+      sqlScript = sqlScript.replace(`"Order"`, `${schemaName}."Order"`);      // const res = await this.prisma.$executeRaw(sqlScript);
       const res = await this.prisma.$executeRawUnsafe(sqlScript);
-
       return res;
     } catch (error) {
-      console.log('🆘 || error:', error);
+      console.log('🆘 || error: runRelations', error);
       return false;
     }
   }
